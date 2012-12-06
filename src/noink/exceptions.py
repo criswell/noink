af1af8c6472bb7ff1d0379029bd1fdc8f960f2ba
Sam Hart <hartsn@gmail.com>
2012-12-06 09:45:49 -0500

    pull in more exceptions
    --HG--
    branch : noink

--- src/noink/exceptions.py
+++ src/noink/exceptions.py
@@ -11,3 +11,20 @@ class DuplicateUser(Exception):
     def __str__(self):
         return repr(self.value)
 
+class DuplicateGroup(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        `return repr(self.value)
+
+class UserNotFound(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
