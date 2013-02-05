bff5deb2086f191d70c950a1cdadfcc4b45f5fe1
Sam Hart <hartsn@gmail.com>
2013-02-05 15:53:40 -0500

    start of a manual test
    --HG--
    branch : noink

---
+++ src/tests/manual_tests/test_SimpleEntries.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+'''
+
+##BOILERPLATE_COPYRIGHT
+##BOILERPLATE_COPYRIGHT_END
+
+'''
+
+from tests.testMain import testMain
+
+entries =
+[
+    (
+        'The Amazing Bambini',
+        "So, my daughter is pretty amazing. She's got all sorts of mad skillz.\n\n" + \
+        "She says 'eww yucky' when I fart. She can climb just about anything. She can " + \
+        "run really fast and hide really well (usually when I am trying to change her " + \
+        "diaper.)\n\n" + \
+        "<!--break-->\n\n" + \
+        "She loves Yo Gabba Gabba, for some silly reason- likely all that communism... " + \
+        "babies <i>love</i> communism, just ask Billy O'!"
+    ),
+    (
+        'Rabbit Rabbit Rabbit',
+        "Rabbit rabbit rabbit, rabbit rabbit rabbit rabbit? Rabbit, rabbit rabbit. " + \
+        "Rabbit! Rabbit, rabbit, rabbit!\n\n" + \
+        "Rabbit rabbit rabbit rabbit..... rabbit... rabbit rabbit, rabbit? Rabbit- rabbit " + \
+        "rabbit rabbit. Rabbit rabbit rabbit rabbit rabbit rabbit rabbit. Rabbit rabbit " + \
+        "rabbit rabbit. <!--break--> Rabbit, rabbit? Rabbit, rabbit!\n\n" + \
+        "Rabbit rabbit rabbit rabbit rabbit rabbit cheesecake artichoke!"
+    )
+]
+
+from noink.userDB import UserDB
+from noink.entryDB import EntryDB
+
+class SimpleEntries:
+
+    def __init__(self):
+        self.testMain = testMain()
+
+        self.userDB = UserDB()
+        self.entryDB = EntryDB()
+
+        u = userDB.add("criswell", "Sam Hart")
+
+
+    def __del__(self):
+        del(self.testMain)
+
