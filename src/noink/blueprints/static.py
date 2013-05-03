b342e67fe01c3b53a9d63e0e2ebc4ea50a57ed4b
Sam Hart <hartsn@gmail.com>
2013-05-02 22:35:25 -0400

    add header to node, remove debugging
    --HG--
    branch : noink

--- src/noink/blueprints/static.py
+++ src/noink/blueprints/static.py
@@ -13,7 +13,5 @@ staticPage = Blueprint('static', __name__, mainApp.config['STATIC_PATH'])
 
 @staticPage.route("/s/<path:filename>")
 def staticShow(filename):
-    print mainApp.config['STATIC_PATH']
-    print filename
     return send_file(safe_join(mainApp.config['STATIC_PATH'], filename))
 
