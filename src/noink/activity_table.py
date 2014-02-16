d9824128eabfbc4c464683ffc940e565dcc9ef3c
Sam Hart <hartsn@gmail.com>
2014-02-15 23:24:09 -0500

    this seems cleaner
    --HG--
    branch : noink

--- src/noink/activity_table.py
+++ src/noink/activity_table.py
@@ -15,7 +15,4 @@ __activities = [
     'edit_self'
 ]
 
-# FXIME- Fugly, probably too ineffecient even for something not designed
-# for efficiency...
-__tact = dict(enumerate(__activities, start=1))
-activity_table = dict(zip(__tact.values(), __tact.keys()))
+activity_table = dict((v,k) for k,v in enumerate(__activities, start=1))
