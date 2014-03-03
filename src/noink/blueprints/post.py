74b32c436f5b4080a4982713063024bad81736cf
Sam Hart <hartsn@gmail.com>
2014-03-03 16:04:52 -0500

    parent makes more sense to reference the parent entry
    --HG--
    branch : noink

--- src/noink/blueprints/post.py
+++ src/noink/blueprints/post.py
@@ -25,6 +25,7 @@ def new_post():
     """
     user_db = UserDB()
     role_db = RoleDB()
+    entry_db = EntryDB()
 
     if current_user.is_authenticated() and current_user.is_active():
         #is_admin = user_db.in_group(current_user, mainApp.config['ADMIN_GROUP'])
@@ -45,13 +46,15 @@ def new_post():
 
         parent_group = None
         if request.values.has_key('parent'):
-            parent_group = user_db.get_group(request.values['parent'])
+            parent = entry_db.find_by_id(request.values['parent'])
+            parent_group = parent.group
 
         # If everything else fails, we default to the top level
         if parent_group is None:
             parent_group = user_db.get_group(mainApp.config['TOP_LEVEL_GROUP'])
 
         if parent_group in avail_groups:
+
             return render_template('new_post.html', state=get_state(), groups=groups)
         else:
             return render_template('noink_message.html', state=get_state(),
