b86417974bbb81b137d03d69b0a8297f0a767a0e
Sam Hart <hartsn@gmail.com>
2014-03-28 14:00:57 -0400

    fix setpath so it plays nice with zsh. begin to refactor admin user
    --HG--
    branch : noink

--- src/setpath.sh
+++ src/setpath.sh
@@ -4,11 +4,13 @@
 # If a parameter is provided, it assumes it's something to be sourced before
 # (such as a virtualenv activate script)
 
-MY_CWD=`pwd`
+MY_CWD=$(pwd)
 
 if [[ -n "$1" ]]; then
     source $1
 fi
 
 export PYTHONPATH=$PYTHONPATH:${MY_CWD}
+
+unset MY_CWD
 
