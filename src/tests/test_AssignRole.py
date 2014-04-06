0c382714213e7f3995d9106d76be2024b2e4a8aa
Sam Hart <hartsn@gmail.com>
2014-04-06 12:47:47 -0400

    finish assign role
    --HG--
    branch : noink

--- src/tests/test_AssignRole.py
+++ src/tests/test_AssignRole.py
@@ -8,18 +8,21 @@ import unittest
 from testRoot import RootClass
 
 from noink.user_db import UserDB
+from noink.role_db import RoleDB
 from noink.activity_table import get_activity_dict
 
 class AssignRole(RootClass):
 
     def test_AssignRole(self):
         user_db = UserDB()
+        role_db = RoleDB()
         u = user_db.add("jontest", "pass", "Jon Q. Testuser")
         g = user_db.add_group('test_group')
-        gid = g.id
         user_db.add_to_group(u, g)
-        groups = set(user_db.get_users_groups(u))
-        self.assertTrue(g in groups)
+        r = role_db.add_role('test_role', 'test role', get_activity_dict(True))
+        role_db.assign_role(u, g, r)
+        all_roles = set(rm.role for rm in role_db.get_roles(u))
+        self.assertTrue(r in all_roles)
 
 if __name__ == '__main__':
     unittest.main()
