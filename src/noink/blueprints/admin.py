de7a75a81462a151802784b54414bc3c2d87e7c2
Sam Hart <hartsn@gmail.com>
2014-04-07 20:45:13 -0400

    start refactoring admin
    --HG--
    branch : noink

--- src/noink/blueprints/admin.py
+++ src/noink/blueprints/admin.py
@@ -24,9 +24,10 @@ def admin_page():
     user_db = UserDB()
 
     if current_user.is_authenticated() and current_user.is_active():
-        return render_template('admin.html',
-            state=get_state(),
-            admin=user_db.in_group(current_user, mainApp.config['ADMIN_GROUP']))
+        links = {}
+        is_admin = user_db.in_group(current_user, mainApp.config['ADMIN_GROUP'])
+        return render_template('admin.html', state=get_state(), links=links,
+                is_admin=is_admin)
 
     return render_template('noink_message.html', state=get_state(),
         title=_('Not authorized'),
