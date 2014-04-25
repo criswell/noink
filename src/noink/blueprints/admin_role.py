0fb2ea51a7db3a55988afdf1a75d739af2d6936a
Sam Hart <hartsn@gmail.com>
2014-04-25 18:09:16 -0400

    start fleshing out role admin
    --HG--
    branch : noink

--- src/noink/blueprints/admin_role.py
+++ src/noink/blueprints/admin_role.py
@@ -10,7 +10,6 @@ from noink import mainApp, _
 from noink.state import get_state
 from noink.user_db import UserDB
 from noink.role_db import RoleDB
-from noink.exceptions import GroupNotFound, DuplicateGroup
 
 from noink.blueprints.admin_user import _not_auth
 
@@ -22,4 +21,27 @@ def admin_role_page(rid):
     """
     Renders the role admin page
     """
-    pass
+    user_db = UserDB()
+    role_db = RoleDB()
+
+    if current_user.is_authenticated() and current_user.is_active():
+        is_admin = user_db.in_group(current_user, mainApp.config['ADMIN_GROUP'])
+        all_activities = set()
+        for m in role_db.get_roles(current_user):
+            acts = role_db.get_activities(m.role_id)
+            for act in acts:
+                if acts[act]:
+                    all_activities.add(act)
+
+        can_view_roles = 'view_roles' in all_activities
+        can_edit_roles = 'edit_roles' in all_activities
+
+        if is_admin or can_view_roles:
+            if rid is None:
+                pass
+            else:
+                pass
+        else:
+            return _not_auth()
+    else:
+        return _not_auth()
