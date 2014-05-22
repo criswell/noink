d57486d7e12fddac6c86d641f208292d5d45aad8
Sam Hart <hartsn@gmail.com>
2014-05-22 16:53:24 -0400

    need to ignore this in icebox mode
    --HG--
    branch : noink

--- src/noink/blueprints/utils.py
+++ src/noink/blueprints/utils.py
@@ -4,11 +4,13 @@ from flask import request, redirect, url_for
 from flask.ext.login import current_user
 
 from noink import mainApp
+from noink.state import get_state
 
 def check_login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if mainApp.config['REQUIRE_LOGIN_FOR_DYNAMIC']:
+        s = get_state()
+        if mainApp.config['REQUIRE_LOGIN_FOR_DYNAMIC'] and not s.icebox:
             if not current_user.is_authenticated() or not \
                     current_user.is_active():
                 return redirect(url_for('login.login_page', next=request.url))
